a79e642bda57795ed76b2e1995d2fb85ea7396f2
Oscar Umana <oscarumana1021@gmail.com>
2024-01-06 15:02:05 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <h1 class="text-3xl">Welcome to {{title}}!</h1>
+ 
     <router-outlet></router-outlet>
   `,
   styleUrl: './app.component.css'
